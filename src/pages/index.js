05e98347b7c4c95064a91adfdfc59a344e89f8ef
hermipkevehs <hermipkevehs@gmail.com>
2021-02-10 20:27:36 +0800

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Layout from "../components/Layout";
+import HomeContact from "../components/HomeContact";
+import Header from "../components/Header";
+
+const index = () => {
+   return (
+      <Layout>
+         <Header />
+         <HomeContact />
+      </Layout>
+   );
+};
+
+export default index;
